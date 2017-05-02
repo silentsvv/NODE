d578ac7486b4d3dd6a2573617879105cc5a43035
silentsvv <silentsvv@163.com>
2017-05-02 23:30:01 +0800

    读取翻页数据成功

--- index.js
+++ index.js
@@ -4,46 +4,50 @@ var cheerio = require('cheerio');
 var fs = require('fs')
 
 var driver = new webdriver.Builder()
-  .forBrowser('phantomjs')
+  .forBrowser('chrome')
   .build();
 
-/*function RouterPage () {
-  var title = null
-     ,imgSrc = null
-     ,link = null;
+function RouterPage () {
+  var page = (function () {
+    this.title = null
+    this.imgSrc = null
+    this.link = null
 
-  this.setTitle = function (newTitle) {
-    title = newTitle
-  }
+    this.setTitle = function (newTitle) {
+      this.title = newTitle
+    }
 
-  this.getTitle = function () {
-    return title
-  }
+    this.getTitle = function () {
+      return this.title
+    }
 
-  this.setImgSrc = function (newImgSrc) {
-    imgSrc = newImgSrc
-  }
+    this.setImgSrc = function (newImgSrc) {
+      this.imgSrc = newImgSrc
+    }
 
-  this.getImgSrc = function () {
-    return imgSrc
-  }
+    this.getImgSrc = function () {
+      return this.imgSrc
+    }
 
-  this.setLink = function (newLink) {
-    link = newLink
-  }
+    this.setLink = function (newLink) {
+      this.link = newLink
+    }
 
-  this.getLink = function () {
-    return link
-  }
-
-}*/
-
-function RouterPage (title,imgSrc,link) {
-  this.title = title
-  this.imgSrc = imgSrc
-  this.link = link
+    this.getLink = function () {
+      return this.link
+    }
+    return {
+      'setTitle': this.setTitle,
+      'getTitle': this.getTitle,
+      'setImgSrc': this.setImgSrc,
+      'getImgSrc': this.getImgSrc,
+      'setLink': this.setLink,
+      'getLink': this.getLink
+    }
+  })()
+  return page;
 }
-
+var newImgArr = [];
 driver.get('http://tu.duowan.com/tu')
 var source = driver.getPageSource().then(function (src) {
     fs.writeFile('message.txt',src, (err)=>{
@@ -52,29 +56,69 @@ var source = driver.getPageSource().then(function (src) {
     })
     fs.readFile('message.txt','utf-8',(err,data)=>{
       if(err) throw err
-      var newImgArr = [];
       var html = data
       var $ = cheerio.load(html)
       var li = $('.masonry-brick:not(.tags) a');
       var img = li.find('img')
       if(img.length > 0){
         img.each(function(i,elem){
-          let rPage = new RouterPage()
+          var rPage = new RouterPage()
           let parent = $(this).closest('li')
           let src = $(this).attr('src')
           let title = parent.find('em a').text()
           let link = parent.find('em a').attr('href')
-          rPage.title = title
-          rPage.imgSrc = src
-          rPage.link = link
+          rPage.setTitle(title)
+          rPage.setImgSrc(src)
+          rPage.setLink(link)
           newImgArr.push(rPage)
         })
-        for(let i in newImgArr) {
-          console.log(newImgArr[i])
+        console.log(newImgArr.length+"长度")
+        for(let i = 0, max = newImgArr.length; i < max; i++) {
+          console.log(newImgArr[i].getTitle())
         }
       }else{
         console.log("-->masonry-brick-->找不到相关类信息")
       }
     })
-    
+})
+
+var source2 = driver.executeScript("var winH = document.body.scrollHeight;window.scrollTo(0,winH)").then(function(){
+  driver.sleep(5000);
+  var newHtml = driver.getPageSource().then(function(src){
+    fs.writeFile('message2.txt',src, (err)=>{
+      if(err) throw err
+      console.log("It's saved")
+    })
+    fs.readFile('message2.txt','utf-8',(err,data)=>{
+      if(err) throw err
+      var html = data
+      var $ = cheerio.load(html)
+      var li = $('.masonry-brick:not(.tags) a');
+      var img = li.find('img')
+      if(img.length > 0){
+        img.each(function(i,elem){
+          var rPage = new RouterPage()
+          let parent = $(this).closest('li')
+          let src = $(this).attr('src')
+          let title = parent.find('em a').text()
+          let link = parent.find('em a').attr('href')
+          let isExist = newImgArr.find((item)=>
+            item.title == title
+          )
+          if(!isExist){
+            rPage.setTitle(title)
+            rPage.setImgSrc(src)
+            rPage.setLink(link)
+            newImgArr.push(rPage)
+          }
+        })
+        for(let i = 0, max = newImgArr.length; i < max; i++) {
+          console.log(newImgArr[i].getTitle())
+        }
+        console.log(newImgArr.length+"长度")
+      }else{
+        console.log("-->masonry-brick-->找不到相关类信息")
+      }
+    })
+  })
 })
