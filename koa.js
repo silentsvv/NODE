9883a9d57d34d47ec0d08db5adc77b57a25485b2
silentsvv <silentsvv@163.com>
2017-05-24 00:02:39 +0800

    调整结构

--- koa.js
+++ koa.js
@@ -3,12 +3,28 @@ const app = new Koa()
 const port = process.env.port || 3000
 const logger = require('koa-logger')
 const bodyparser = require('koa-bodyparser')
-const routes = require('./routes')
 
+
+const api = require('./index.js')
+const Router = require('koa-router')
+const router = new Router()
 
 app.use(logger())
 app.use(bodyparser())
-app.use(routes.routes(), routes.allowedMethods())
+app.use(router.routes(), router.allowedMethods())
+
+router.get('/hello', async (ctx) => {
+  let p = api()
+  let data = await p
+  console.log(data)
+  ctx.body = data
+  // console.log(data)
+  // ctx.body = data
+})
+
+
+
+
 
 const server = app.listen(port)
 
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,14 @@
 const Router = require('koa-router')
 const router = new Router()
+const Api = require(__dirname+'/index.js')
 
 router.get('/hello', async (ctx) => {
-  ctx.body = 'heloo'
+  let data
+  // ctx.body = 'heloo'
+  let a = Api()
+  await a
+  a.then((res) => console.log(ctx.body = data))
+  console.log(a)
 })
 
 module.exports = router
